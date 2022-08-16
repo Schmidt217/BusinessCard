929160bab59bcdb912b12a5cb66ae1f6b159e126
Mike Schmidt <schmidt217@gmail.com>
2022-08-16 13:51:57 -0600

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Michael Schmidt on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
